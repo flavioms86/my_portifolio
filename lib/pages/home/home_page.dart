a7d32df8bb1a2a96f810f4569944f48435a1dcab
Flavio Silva <flavioms86@hotmail.com>
2022-07-22 15:50:32 -0300

    Widget do card inicial separado em outro arquivo

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:my_portifolio/widgets/Cards/intro_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Center(
+        child: IntroCard(text: "Flavio Silva"),
+      ),
+    );
   }
 }
---
+++ lib/widgets/Cards/intro_card.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class IntroCard extends StatelessWidget {
+  final String text;
+  const IntroCard({required this.text, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(25.0),
+      padding: const EdgeInsets.all(25.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+              color: Colors.black.withOpacity(.20),
+              offset: const Offset(0, 20),
+              blurRadius: 25),
+        ],
+        border: Border.all(color: Colors.black, width: 3),
+        borderRadius: BorderRadius.circular(25.0),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey.shade700),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
