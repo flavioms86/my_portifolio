59e3d859586ee2674790bb97c7a100019e389d85
Flavio Silva <flavioms86@hotmail.com>
2022-07-29 15:26:49 -0300

    Widget cabeçalho criado

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_portifolio/widgets/Cards/intro_card.dart';
+import 'package:my_portifolio/widgets/Header/header.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,36 +16,7 @@ class HomePage extends StatelessWidget {
             padding: const EdgeInsets.all(15.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: const [
-                        Text(
-                          "Bem-vindo de volta 👋",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w300),
-                        ),
-                        Text(
-                          "Portifólio do Flávio",
-                          style: TextStyle(
-                              fontSize: 28, fontWeight: FontWeight.bold),
-                        )
-                      ],
-                    ),
-                    const Icon(
-                      Icons.account_circle,
-                      color: Colors.black,
-                      size: 30,
-                    ),
-                  ],
-                ),
-              ],
+              children: const [Header()],
             ),
           ),
         ),
---
+++ lib/widgets/Header/header.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  const Header({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            Text(
+              "Bem-vindo de volta 👋",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
+            ),
+            Text(
+              "Portifólio do Flávio",
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+        const Icon(
+          Icons.account_circle,
+          color: Colors.black,
+          size: 30,
+        ),
+      ],
+    );
+  }
+}
