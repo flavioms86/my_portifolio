7b72b3d8ee1bd02d647b339a4af375a335099fdb
Flavio Silva <flavioms86@hotmail.com>
2022-07-29 16:16:31 -0300

    Adicionado Project Card inicial

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -16,10 +16,90 @@ class HomePage extends StatelessWidget {
             padding: const EdgeInsets.all(15.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [Header()],
+              children: const [
+                Header(),
+                SizedBox(height: 25),
+                Text(
+                  "Projetos",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
+                ProjectCard()
+              ],
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class ProjectCard extends StatelessWidget {
+  const ProjectCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 350,
+      padding: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15.0),
+        boxShadow: [
+          BoxShadow(
+            offset: const Offset(5, 15),
+            color: Colors.black.withOpacity(.15),
+            blurRadius: 15,
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "Meu Projeto",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                padding: const EdgeInsets.all(5),
+                child: const Text(
+                  "2022",
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Expanded(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image.network(
+                "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+                fit: BoxFit
+                    .cover, // To cover the entire Area maintain aspect ratio
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            """Texto a ser inserido para descrever o projeto em questão. Informações essenciais para ter uma dimensão exata do que se trata o projeto. Texto a ser inserido """,
+            maxLines: 3,
+            style: TextStyle(fontSize: 14, overflow: TextOverflow.ellipsis),
+          )
+        ],
       ),
     );
   }
