de3aa28e3e4c8df12eb78fcdb17d1ae3cdb7463d
Flavio Silva <flavioms86@hotmail.com>
2022-07-31 09:46:04 -0300

    Adicionado avatar no icone do cabeçalho

--- lib/widgets/Header/header.dart
+++ lib/widgets/Header/header.dart
@@ -24,11 +24,7 @@ class Header extends StatelessWidget {
             )
           ],
         ),
-        const Icon(
-          Icons.account_circle,
-          color: Colors.black,
-          size: 30,
-        ),
+        Image.asset("assets/img/avataaars.png", height: 45),
       ],
     );
   }
